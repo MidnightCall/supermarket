2eaddae2df4880b29cd19b261382664fab8674c0
kojikoji <1310402980@qq.com>
2022-07-12 14:05:15 +0800

    2022/7/12 14：04  commit

--- login.c
+++ login.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "login.h"
 
 /* 局部函数模型 */
 static void showMenu();
 static void showChar(char ch, int n);
+static char* stringGet(char *st,int n);
 
 /*
 * 操作：运行登录/注册系统
@@ -20,23 +22,78 @@ void runLogIn()
 */
 int getChoice()
 {
-	showMenu();
-	return 0;
+	int choice;
+
+	do
+	{
+		showMenu();
+		scanf("%d", &choice);
+	} while (choice > 3 || choice < 1);
+
+	return choice;
+}
+
+/*
+* 操作：注册账号
+*/
+void registration()
+{
+	UI account;
+	static int currentAccount = 10000;
+	int firstPassword;
+	int secondPassword;
+
+	printf("你的账号是%d\n", currentAccount);
+	account.id = currentAccount++;
+	do {
+		printf("请输入你的密码:");
+		stringGet(firstPassword, PSIZE);
+		printf("请确认你的密码:");
+		stringGet(secondPassword, PSIZE);
+		if (firstPassword == secondPassword) {
+			strcpy(account.password, firstPassword);
+			break;
+		}
+	} while (1);
+	printf("注册成功!\n");
+	fwrite()
 }
 
 /* 局部函数定义 */
 static void showMenu()
 {
 	showChar('-', 45);
+	putchar('\n');
 	printf("欢迎来到超市管理系统，请选择你要进行的操作:\n");
 	printf("1.登录\n");
 	printf("2.注册\n");
 	printf("3.退出\n");
 	showChar('-', 45);
+	putchar('\n');
 }
 
 static void showChar(char ch, int n)
 {
 	for (int i = 0; i < n; i++)
-		printf(ch);
+		putchar(ch);
+}
+
+static char* stringGet(char* st, int n)
+{
+	char* ret_val;
+	char* find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val)
+	{
+		find = strchr(st, '\n');
+		if (find){
+			*find = '\0';
+		}
+		else {
+			FLUSH;
+		}
+	}
+
+	return ret_val;
 }
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "login.h"
 
 int main(void)
 {
+	srand((unsigned)time(NULL));
 	runLogIn();
 }
 
